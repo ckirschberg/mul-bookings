c84375b11f8bb31192c21b50f8d22ffc239809dd
Christian Kirschberg <ckirschberg@gmail.com>
2022-11-14 11:08:21 +0100

    map

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import BookingForm from './BookingForm';
 
 function App() {
   return (
-<div className="App">
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
